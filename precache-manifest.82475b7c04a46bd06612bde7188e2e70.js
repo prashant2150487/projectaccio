8de479d86b7a7b36c0048c4a8b17ee78eaa4e3e1
prashant2150487 <you@example.com>
2022-10-02 20:12:56 +0530

    Updates

---
+++ precache-manifest.82475b7c04a46bd06612bde7188e2e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57b23fed17bb77baad14",
+    "url": "/projectaccio/static/css/main.2a390fd1.chunk.css"
+  },
+  {
+    "revision": "57b23fed17bb77baad14",
+    "url": "/projectaccio/static/js/main.57b23fed.chunk.js"
+  },
+  {
+    "revision": "8df31bdc3c2ab8b36b0f",
+    "url": "/projectaccio/static/js/1.8df31bdc.chunk.js"
+  },
+  {
+    "revision": "e9d1ff06f49058b061ee",
+    "url": "/projectaccio/static/js/runtime~main.e9d1ff06.js"
+  },
+  {
+    "revision": "8a8cad44f82b699ab862298578145e9a",
+    "url": "/projectaccio/static/media/technology 1.8a8cad44.png"
+  },
+  {
+    "revision": "af377c9b3a2ac8a2626960b2c7d08252",
+    "url": "/projectaccio/index.html"
+  }
+];
